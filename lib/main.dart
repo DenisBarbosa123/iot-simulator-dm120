147e6545021b7ceddc1db510901fa930d36ea120
Denis Barbosa <denis_barbosa123@hotmail.com>
2023-03-13 20:54:14 -0300

    Add thermometer which will show the temperature by getting the value from MQTT server

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,174 @@
-import 'dart:async';
 import 'dart:io';
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'thermometer_widget.dart';
 
-final client = MqttServerClient('test.mosquitto.org', '');
 
-var pongCount = 0; // Pong counter
+void main() => runApp(const MyApp());
 
-Future<int> main() async {
-  client.logging(on: true);
-  client.setProtocolV311();
-  client.keepAlivePeriod = 20;
-  client.connectTimeoutPeriod = 2000; // milliseconds
-  client.onDisconnected = onDisconnected;
-  client.onConnected = onConnected;
-  client.onSubscribed = onSubscribed;
-  client.pongCallback = pong;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  final connMess = MqttConnectMessage()
-      .withClientIdentifier('Mqtt_MyClientUniqueId')
-      .withWillTopic('willtopic') // If you set this you must set a will message
-      .withWillMessage('My Will message')
-      .startClean() // Non persistent session for testing
-      .withWillQos(MqttQos.atLeastOnce);
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
 
-  print('EXAMPLE::Mosquitto client connecting....');
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
 
-  client.connectionMessage = connMess;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-  try {
-    await client.connect();
-  } on NoConnectionException catch (e) {
-    // Raised by the client when connection fails.
-    print('EXAMPLE::client exception - $e');
-    client.disconnect();
-  } on SocketException catch (e) {
-    // Raised by the socket layer
-    print('EXAMPLE::socket exception - $e');
-    client.disconnect();
+class _MyHomePageState extends State<MyHomePage> {
+
+  final client = MqttServerClient('test.mosquitto.org', '');
+
+  var pongCount = 0;
+
+  double _temp = 20;
+  double _hum = 50;
+
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) => _connect());
   }
 
-  if (client.connectionStatus!.state == MqttConnectionState.connected) {
-    print('EXAMPLE::Mosquitto client connected');
-  } else {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: SizedBox(
+          child: ThermometerWidget(
+          borderColor: Colors.red,
+          innerColor: Colors.green,
+          indicatorColor: Colors.red,
+          temperature: _temp
+    ),
+    ),
+    ),
+
+    );
+  }
+
+  _connect() async {
+    client.logging(on: true);
+    client.setProtocolV311();
+    client.keepAlivePeriod = 20;
+    client.connectTimeoutPeriod = 2000; // milliseconds
+    client.onDisconnected = onDisconnected;
+    client.onConnected = onConnected;
+    client.onSubscribed = onSubscribed;
+    client.pongCallback = pong;
+
+    final connMess = MqttConnectMessage()
+        .withClientIdentifier('Mqtt_MyClientUniqueId')
+        .withWillTopic(
+        'willtopic') // If you set this you must set a will message
+        .withWillMessage('My Will message')
+        .startClean() // Non persistent session for testing
+        .withWillQos(MqttQos.atLeastOnce);
+
+    print('EXAMPLE::Mosquitto client connecting....');
+
+    client.connectionMessage = connMess;
+
+    try {
+      await client.connect();
+    } on NoConnectionException catch (e) {
+      // Raised by the client when connection fails.
+      print('EXAMPLE::client exception - $e');
+      client.disconnect();
+    } on SocketException catch (e) {
+      // Raised by the socket layer
+      print('EXAMPLE::socket exception - $e');
+      client.disconnect();
+    }
+
+    if (client.connectionStatus!.state == MqttConnectionState.connected) {
+      print('EXAMPLE::Mosquitto client connected');
+    } else {
+      print(
+          'EXAMPLE::ERROR Mosquitto client connection failed - disconnecting, status is ${client
+              .connectionStatus}');
+      client.disconnect();
+      exit(-1);
+    }
+
+    print('EXAMPLE::Subscribing to the wokwi-iot-simulator-dm120/test');
+    const topic = 'wokwi-iot-simulator-dm120/test';
+    client.subscribe(topic, MqttQos.atMostOnce);
+
+    client.updates!.listen((List<MqttReceivedMessage<MqttMessage?>>? c) {
+      final recMess = c![0].payload as MqttPublishMessage;
+      final pt =
+      MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+
+      print(
+          'EXAMPLE::Change notification:: topic is <${c[0]
+              .topic}>, payload is <-- $pt -->');
+      print('');
+
+      Map responsePayload = json.decode(pt);
+      print(responsePayload);
+
+      setState(() {
+        _temp = double.parse(responsePayload['temperature'].toString());
+        _hum = double.parse(responsePayload['humidity'].toString());
+      });
+    });
+
+    return 0;
+  }
+
+  /// The subscribed callback
+  void onSubscribed(String topic) {
+    print('EXAMPLE::Subscription confirmed for topic $topic');
+  }
+
+  /// The unsolicited disconnect callback
+  void onDisconnected() {
+    print('EXAMPLE::OnDisconnected client callback - Client disconnection');
+    if (client.connectionStatus!.disconnectionOrigin ==
+        MqttDisconnectionOrigin.solicited) {
+      print('EXAMPLE::OnDisconnected callback is solicited, this is correct');
+    } else {
+      print(
+          'EXAMPLE::OnDisconnected callback is unsolicited or none, this is incorrect - exiting');
+      exit(-1);
+    }
+    if (pongCount == 3) {
+      print('EXAMPLE:: Pong count is correct');
+    } else {
+      print('EXAMPLE:: Pong count is incorrect, expected 3. actual $pongCount');
+    }
+  }
+
+  /// The successful connect callback
+  void onConnected() {
     print(
-        'EXAMPLE::ERROR Mosquitto client connection failed - disconnecting, status is ${client.connectionStatus}');
-    client.disconnect();
-    exit(-1);
+        'EXAMPLE::OnConnected client callback - Client connection was successful');
   }
 
-  print('EXAMPLE::Subscribing to the wokwi-iot-simulator-dm120/test');
-  const topic = 'wokwi-iot-simulator-dm120/test';
-  client.subscribe(topic, MqttQos.atMostOnce);
-
-  client.updates!.listen((List<MqttReceivedMessage<MqttMessage?>>? c) {
-    final recMess = c![0].payload as MqttPublishMessage;
-    final pt =
-    MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
-
-    print(
-        'EXAMPLE::Change notification:: topic is <${c[0].topic}>, payload is <-- $pt -->');
-    print('');
-  });
-
-  return 0;
-}
-
-/// The subscribed callback
-void onSubscribed(String topic) {
-  print('EXAMPLE::Subscription confirmed for topic $topic');
-}
-
-/// The unsolicited disconnect callback
-void onDisconnected() {
-  print('EXAMPLE::OnDisconnected client callback - Client disconnection');
-  if (client.connectionStatus!.disconnectionOrigin ==
-      MqttDisconnectionOrigin.solicited) {
-    print('EXAMPLE::OnDisconnected callback is solicited, this is correct');
-  } else {
-    print(
-        'EXAMPLE::OnDisconnected callback is unsolicited or none, this is incorrect - exiting');
-    exit(-1);
+  /// Pong callback
+  void pong() {
+    print('EXAMPLE::Ping response client callback invoked');
+    pongCount++;
   }
-  if (pongCount == 3) {
-    print('EXAMPLE:: Pong count is correct');
-  } else {
-    print('EXAMPLE:: Pong count is incorrect, expected 3. actual $pongCount');
-  }
-}
-
-/// The successful connect callback
-void onConnected() {
-  print(
-      'EXAMPLE::OnConnected client callback - Client connection was successful');
-}
-
-/// Pong callback
-void pong() {
-  print('EXAMPLE::Ping response client callback invoked');
-  pongCount++;
 }
